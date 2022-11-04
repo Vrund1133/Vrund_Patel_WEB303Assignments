f8aed0b5d6b327be9c26a4445ed4f6b11d411815
Vrund1133 <105453398+Vrund1133@users.noreply.github.com>
2022-11-03 21:42:44 -0400

    Assignment6

---
+++ Assignment6/js/script.js
@@ -0,0 +1,29 @@
+
+$(function () {
+    $('.accordion .accordion-control').on('click', function (event) {
+        event.preventDefault();
+            $('.accordion-control').toggleClass('active');
+            $(this).next('.accordion-panel').not(':animated').slideToggle();
+            $(this).siblings('.accordion-panel').not(':animated').hide("slow")
+
+    });
+ 
+    
+  $(".tab-list").each(function () {
+    var $this = $(this); 
+    var $tab = $this.find("li.active"); 
+    var $link = $tab.find("a"); 
+    var $panel = $($link.attr("href")); 
+    $this.on("click", ".tab-control", function (e) {
+      e.preventDefault(); 
+      var $link = $(this);
+      var id = this.hash; 
+      if (id && !$link.parent().is(".active")) {
+        $panel.removeClass("active");
+        $tab.removeClass("active"); 
+        $panel = $(id).addClass("active"); 
+        $tab = $link.parent().addClass("active"); 
+      }
+    });
+  });
+});
